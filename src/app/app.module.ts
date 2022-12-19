e382b5461d85a14c77b45fdaf76d35b88e30f2b7
abdelhamed <abdelhamedreda@outlook.com>
2022-12-19 17:50:34 +0200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SingInComponent } from './sing-in/sing-in.component';
+import { MasterThemeComponent } from './master-theme/master-theme.component';
+import { ComponentsComponent } from './components/components.component';
+import { PrototypeComponent } from './prototype/prototype.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingInComponent,
+    MasterThemeComponent,
+    ComponentsComponent,
+    PrototypeComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
