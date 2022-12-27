32c46f3264b6edaed9048dcbfca44c508bb28fb0
abdelhamed <abdelhamedreda@outlook.com>
2022-12-27 17:11:09 +0200

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ResourcesComponent } from './resources/resources.component';
 import { ManufacturingComponent } from './manufacturing/manufacturing.component';
 import { MachinesComponent } from './machines/machines.component';
 import { PlanningComponent } from './planning/planning.component';
+import { NavComponent } from './nav/nav.component';
 
 
 
@@ -47,6 +48,7 @@ const appRoute: Routes =[
     ManufacturingComponent,
     MachinesComponent,
     PlanningComponent,
+    NavComponent,
     
     
     
